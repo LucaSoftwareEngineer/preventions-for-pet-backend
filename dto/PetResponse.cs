b6e1e4fd7156579966fab2f8883e16c998d10dd6
LucaSoftwareEngineer <santoro.luca.ing@gmail.com>
2026-06-13 13:27:50 +0200

    feat: add dto PetResponse & ProprietarioResponse

---
+++ dto/PetResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace dto
+{
+    public record PetResponse
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public int Eta { get; set; }
+
+        public DateTime DataNascita { get; set; }
+
+        private ProprietarioResponse proprietario { get; set; }
+    }
+}
---
+++ dto/ProprietarioResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace dto
+{
+    public record ProprietarioResponse
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cognome { get; set; }
+
+        public List<PetResponse> Pets { get; set; } = new List<PetResponse>();
+    }
+}
