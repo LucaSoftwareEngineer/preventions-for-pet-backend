67bc29610adbf4314013cd5748e84daf217cc597
LucaSoftwareEngineer <lucapc40@gmail.com>
2026-06-13 11:36:06 +0200

    feat: add PetRepository & ProprietarioRepository

---
+++ repository/PetRepository.cs
@@ -0,0 +1,41 @@
+﻿using data;
+using model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace repository
+{
+    public class PetRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public PetRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public List<Pet> findAll() {
+            return _appDbContext.pets.ToList();
+        }
+
+        public Pet? findById(int id) {
+            return _appDbContext.pets.FirstOrDefault(p => p.Id == id);
+        }
+
+        public Pet save(Pet pet) { 
+            if (pet.Id != 0) {
+                return _appDbContext.pets.Update(pet).Entity;
+            } else {
+                return _appDbContext.pets.Add(pet).Entity;
+            }
+        }
+
+        public void deleteById(int id) {
+            Pet? pet = findById(id);
+            if (pet != null) {
+                _appDbContext.pets.Remove(pet);
+            }
+        }
+    }
+}
---
+++ repository/ProprietarioRepository.cs
@@ -0,0 +1,49 @@
+﻿using data;
+using model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace repository
+{
+    public class ProprietarioRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public ProprietarioRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public List<Proprietario> findAll()
+        {
+            return _appDbContext.proprietari.ToList();
+        }
+
+        public Proprietario? findById(int id)
+        {
+            return _appDbContext.proprietari.FirstOrDefault(p => p.Id == id);
+        }
+
+        public Proprietario save(Proprietario proprietario)
+        {
+            if (proprietario.Id != 0)
+            {
+                return _appDbContext.proprietari.Update(proprietario).Entity;
+            }
+            else
+            {
+                return _appDbContext.proprietari.Add(proprietario).Entity;
+            }
+        }
+
+        public void deleteById(int id)
+        {
+            Proprietario? proprietario = findById(id);
+            if (proprietario != null)
+            {
+                _appDbContext.proprietari.Remove(proprietario);
+            }
+        }
+    }
+}
